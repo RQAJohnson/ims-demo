07925fb432173fd3d1b64d1eedaaa868f1b4b4c9
RQAJohnson <RQAJohnson@qa.com>
2021-01-13 10:38:40 +0000

    changes to ItemServices

--- src/main/java/com/qa/ims/services/ItemServices.java
+++ src/main/java/com/qa/ims/services/ItemServices.java
@@ -1,6 +1,30 @@
 package com.qa.ims.services;
 
+import java.util.List;
+
 
 public class ItemServices implements CrudServices<Item>{
+	
+private Dao<Item> itemDao;
+	
+	public ItemServices(Dao<Item> itemDao) {
+		this.itemDao = itemDao;
+	}
+	
+	public List<Item> readAll() {
+		return itemDao.readAll();
+	}
+
+	public Item create(Item item) {
+		return itemDao.create(item);
+	}
+
+	public Item update(Item item) {
+		return itemDao.update(item);
+	}
+
+	public void delete(Long id) {
+		itemDao.delete(id);
+	}
 
 }
