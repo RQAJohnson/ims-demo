05c7e7f53526285e79ec11fa085f150698a1421d
RQAJohnson <RQAJohnson@qa.com>
2021-01-13 10:26:20 +0000

    changes to item controller

--- src/main/java/com/qa/ims/controller/itemController.java
+++ src/main/java/com/qa/ims/controller/itemController.java
@@ -8,13 +8,13 @@ import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.services.CrudServices;
 import com.qa.ims.utils.Utils;
 
-public class itemController implements CrudController<item> {
+public class itemController implements CrudController<Item> {
 public static final Logger LOGGER = Logger.getLogger(CustomerController.class);
 	
-	private CrudServices<Customer> customerService;
+	private CrudServices<Item> customerService;
 	
-	public CustomerController(CrudServices<Customer> customerService) {
-		this.customerService = customerService;
+	public ItemController(CrudServices<Item> itemService) {
+		this.itemService = itemService;
 	}
 	
 
@@ -26,42 +26,42 @@ public static final Logger LOGGER = Logger.getLogger(CustomerController.class);
 	 * Reads all customers to the logger
 	 */
 	@Override
-	public List<Customer> readAll() {
-		List<Customer> customers = customerService.readAll();
-		for(Customer customer: customers) {
-			LOGGER.info(customer.toString());
+	public List<Item> readAll() {
+		List<Item> items = itemService.readAll();
+		for(Item items: items) {
+			LOGGER.info(item.toString());
 		}
-		return customers;
+		return items;
 	}
 
 	/**
 	 * Creates a customer by taking in user input
 	 */
 	@Override
-	public Customer create() {
+	public Item create() {
 		LOGGER.info("Please enter a first name");
 		String firstName = getInput();
 		LOGGER.info("Please enter a surname");
 		String surname = getInput();
-		Customer customer = customerService.create(new Customer(firstName, surname));
+		Item item = itemService.create(new Item(firstName, surname));
 		LOGGER.info("Customer created");
-		return customer;
+		return item;
 	}
 
 	/**
 	 * Updates an existing customer by taking in user input
 	 */
 	@Override
-	public Customer update() {
+	public Item update() {
 		LOGGER.info("Please enter the id of the customer you would like to update");
 		Long id = Long.valueOf(getInput());
 		LOGGER.info("Please enter a first name");
 		String firstName = getInput();
 		LOGGER.info("Please enter a surname");
 		String surname = getInput();
-		Customer customer = customerService.update(new Customer(id, firstName, surname));
+		Item item = itemService.update(new Item(id, firstName, surname));
 		LOGGER.info("Customer Updated");
-		return customer;
+		return item;
 	}
 
 	/**
@@ -71,7 +71,7 @@ public static final Logger LOGGER = Logger.getLogger(CustomerController.class);
 	public void delete() {
 		LOGGER.info("Please enter the id of the customer you would like to delete");
 		Long id = Long.valueOf(getInput());
-		customerService.delete(id);
+		itemService.delete(id);
 	}
 
 }
