ac9e8102c5c5e5770cfa7792f23ed5a520573ba2
RQAJohnson <RQAJohnson@qa.com>
2021-01-15 12:26:12 +0000

    changes to create method

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -1,5 +1,67 @@
 package com.qa.ims.controller;
 
-public class OrderController {
+import java.util.List;
 
+import org.apache.log4j.Logger;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.services.CrudServices;
+import com.qa.ims.utils.Utils;
+
+public class OrderController implements CrudController<Order> {
+	
+//	inherit CustomerID and ItemID??? 
+//	Garage task!?!?! having an arraylist that adds customer and item to it
+
+public static final Logger LOGGER = Logger.getLogger(OrderController.class);
+	
+	private CrudServices<Order> orderService;
+	
+	public OrderController(CrudServices<Order> orderService) {
+		this.orderService = orderService;
+	}
+	
+	String getInput() {
+		return Utils.getInput();
+	}
+	
+	Long getinpuT() {
+		return Utils.getinpuT();
+	}
+
+	@Override
+	public List<Order> readAll() {
+		List<Order> orders = orderService.readAll();
+		for(Order order: orders) {
+			LOGGER.info(order.toString());
+		}
+		return orders;
+	}
+
+	@Override
+	public Order create() {
+		LOGGER.info("Please Enter Your Customer ID: ");
+		Long id = Long.valueOf(getInput());
+		LOGGER.info("Please Enter the item ID: ");
+		Long itemID = getinpuT();
+		LOGGER.info("Please enter the name of the item you wish to order: ");
+		String title = getInput();
+		Order order = orderService.create(new Order(id, itemID, title));
+		LOGGER.info("Order Placed");
+		return order;
+//		FOR LOOP HERE!!! > 
+	}
+	
+	@Override
+	public void delete() {
+		LOGGER.info("Please Enter the OrderID you would like to delete: ");
+		Long orderID = getinpuT();
+		orderService.delete(orderID);
+		
+	}
+
+	@Override
+	public Order update() {
+		return null;
+	}	
+	
 }
