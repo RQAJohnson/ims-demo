600f582fd8845171fced14e57cc71a84e564c8bd
RQAJohnson <RQAJohnson@qa.com>
2021-01-19 15:47:57 +0000

    no more changes

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -1,21 +1,30 @@
 package com.qa.ims.persistence.domain;
 
 public class Order {
+	
 	private Long orderID;
 	private Long itemID;
 	private Long customerID;
-	private String title;
-	
-	//Need our own attributes
-	//Getters and Setters > customer and item class. 
-	
-	public Order(Long orderID, Long itemID, String title) {
-		super();
+		
+	public Order(Long orderID, Long customerID, Long itemID) {
 		this.orderID = orderID;
 		this.itemID = itemID;
 		this.customerID = customerID;
-		this.title = title;
 	}
+	
+
+	public Order(Long customerID, Long orderID) {
+		this.orderID = orderID;
+		this.customerID = customerID;
+	}
+	public Order(Long customerID) {
+		this.customerID = customerID;
+	}
+
+
+	public Order() {
+	}
+
 
 	public Long getOrderID() {
 		return orderID;
@@ -40,10 +49,48 @@ public class Order {
 	public void setCustomerID(Long customerID) {
 		this.customerID = customerID;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((itemID == null) ? 0 : itemID.hashCode());
+		result = prime * result + ((customerID == null) ? 0 : customerID.hashCode());
+		result = prime * result + ((orderID == null) ? 0 : orderID.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (itemID == null) {
+			if (other.itemID != null)
+				return false;
+		} else if (!itemID.equals(other.itemID))
+			return false;
+		if (customerID == null) {
+			if (other.customerID != null)
+				return false;
+		} else if (!customerID.equals(other.customerID))
+			return false;
+		if (orderID == null) {
+			if (other.orderID != null)
+				return false;
+		} else if (!orderID.equals(other.orderID))
+			return false;
+		return true;
+	}
 	
+}
 	
 
-}
+
 
 
  
