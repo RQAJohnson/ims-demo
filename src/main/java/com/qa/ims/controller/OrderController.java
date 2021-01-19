aac4e7315726b173411a678943dea2531d6a7c1e
RQAJohnson <RQAJohnson@qa.com>
2021-01-19 09:18:16 +0000

    orderline method added

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -1,16 +1,15 @@
 package com.qa.ims.controller;
 
-import java.util.List;
-
+import java.util.ArrayList;
+import java.util.List; 
 import org.apache.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.persistence.domain.Order;
 import com.qa.ims.services.CrudServices;
 import com.qa.ims.utils.Utils;
 
 public class OrderController implements CrudController<Order> {
-	
-//	inherit CustomerID and ItemID??? 
-//	Garage task!?!?! having an arraylist that adds customer and item to it
 
 public static final Logger LOGGER = Logger.getLogger(OrderController.class);
 	
@@ -30,26 +29,59 @@ public static final Logger LOGGER = Logger.getLogger(OrderController.class);
 
 	@Override
 	public List<Order> readAll() {
-		List<Order> orders = orderService.readAll();
-		for(Order order: orders) {
-			LOGGER.info(order.toString());
+		List<Order> order = orderService.readAll();
+		for(Order orders: order) {
+			LOGGER.info(orders.toString());
 		}
-		return orders;
+		return order;
 	}
 
 	@Override
 	public Order create() {
 		LOGGER.info("Please Enter Your Customer ID: ");
 		Long id = Long.valueOf(getInput());
-		LOGGER.info("Please Enter the item ID: ");
-		Long itemID = getinpuT();
-		LOGGER.info("Please enter the name of the item you wish to order: ");
-		String title = getInput();
-		Order order = orderService.create(new Order(id, itemID, title));
-		LOGGER.info("Order Placed");
+		LOGGER.info("Enter the item ID :");
+//		Long itemID = getinpuT();
+		Long orderID = getinpuT();
+		Order order = orderService.create(new Order(id));
+		LOGGER.info("Order Placed!");
 		return order;
-//		FOR LOOP HERE!!! > 
 	}
+	
+//	
+	public Order orderline() {
+		LOGGER.info("Enter the ID of the order you wish to update: ");
+		Long orderID = getinpuT();
+		LOGGER.info("Enter your customer ID: ");
+		Long itemID = Long.valueOf(getInput());
+		Order order = orderService.update(new Order(itemID, orderID));
+		return order;
+//		look up orderID in orders table
+//		
+	}
+	
+	@Override
+	public Order update() {
+		LOGGER.info("Enter the ID of the order you wish to update: ");
+		Long orderID = getinpuT();
+		LOGGER.info("Enter your customer ID: ");
+		Long customerID = Long.valueOf(getInput());
+		Order order = orderService.update(new Order(customerID, orderID));
+		LOGGER.info("Order Successfully Updated");
+		return order;
+	}
+//		<!-------------ADD/DELETE item--------------!>		
+//		List<Order> odr= orderService.update();
+//		Order itemID;
+//		odr.add(itemID);
+//		odr.remove(itemID);		
+////		if order = new order, sysout "do enter Item ID you wish to add.
+////		else "order updated"
+////		else if sysout "enter item ID you wish to remove".
+////		sysout "order updated
+////		return order;
+////		
+
 	
 	@Override
 	public void delete() {
@@ -58,10 +90,5 @@ public static final Logger LOGGER = Logger.getLogger(OrderController.class);
 		orderService.delete(orderID);
 		
 	}
-
-	@Override
-	public Order update() {
-		return null;
-	}	
 	
 }
