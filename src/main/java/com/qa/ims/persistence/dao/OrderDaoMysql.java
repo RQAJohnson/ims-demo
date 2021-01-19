7c1ac16907857f8532414cbda65d99109874f49e
RQAJohnson <RQAJohnson@qa.com>
2021-01-19 09:20:10 +0000

    insert into orderline created

--- src/main/java/com/qa/ims/persistence/dao/OrderDaoMysql.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDaoMysql.java
@@ -1,5 +1,133 @@
 package com.qa.ims.persistence.dao;
 
-public class OrderDaoMysql {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+
+import com.qa.ims.persistence.domain.Order;
+
+public class OrderDaoMysql implements Dao<Order> {
+	
+	public static final Logger LOGGER = Logger.getLogger(ItemDaoMysql.class);
+	private String jdbcConnectionUrl;
+	private String username;
+	private String password;  
+	
+	public OrderDaoMysql(String username, String password) {
+		this.jdbcConnectionUrl = "jdbc:mysql://localhost:3306/ims";
+		this.username = username;
+		this.password = password; 
+		
+		}
+	public OrderDaoMysql(String jdbcConnectionUrl, String username, String password) {
+		this.jdbcConnectionUrl = jdbcConnectionUrl;
+		this.username = username;
+		this.password = password;
+		}	
+	Order orderFromResultSet(ResultSet resultSet) throws SQLException {
+		Long orderID = resultSet.getLong("orderID");
+		Long itemID = resultSet.getLong("itemID");
+		Long customerID = resultSet.getLong("customerID");
+		return new Order(orderID, itemID, customerID);
+	}
+	@Override
+	public List<Order> readAll() {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders");) {
+			ArrayList<Order> order = new ArrayList<>();
+			while (resultSet.next()) {
+				order.add(orderFromResultSet(resultSet));
+			}
+			return order;
+			
+		} catch (SQLException e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+	}
+
+	public Order readLatest() {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders ORDER BY orderID DESC LIMIT 1");) {
+			resultSet.next();
+			return orderFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public Order create(Order order) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("insert into orders(customerID) values(" + order.getCustomerID());
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;	
+	}
+	
+	public Order update(Order order) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("insert into orders(orderID) values(" + order.getOrderID());
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;	
+	}
+	
+	public Order orderline(Order order) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("insert into orderline(ol_orderID, ol_itemID) values(" + order.getOrderID()
+			+ "," + order.getItemID() + ")");
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		} 
+		return null;
+		
+	}
+	public Order readOrder(Long orderID) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders where orderID = " + orderID);) {
+			resultSet.next();
+			return orderFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public void delete(long orderID) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("DELETE FROM orders where orderID = " + orderID);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		System.out.println("Order Deleted");
+	}	
 
 }
