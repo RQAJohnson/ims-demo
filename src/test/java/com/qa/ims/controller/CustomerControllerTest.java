e7df83d3fe1196ccb0d1678def289e914fd0fb50
RQAJohnson <RQAJohnson@qa.com>
2021-01-20 11:10:54 +0000

    checking customer test

--- src/test/java/com/qa/ims/controller/CustomerControllerTest.java
+++ src/test/java/com/qa/ims/controller/CustomerControllerTest.java
@@ -19,45 +19,38 @@ import com.qa.ims.services.CustomerServices;
 @RunWith(MockitoJUnitRunner.class)
 public class CustomerControllerTest {
 	
-	/**
-	 *  The thing I want to fake functionlity for
-	 */
+	
 	@Mock
 	private CustomerServices customerServices;
 	
-	/**
-	 * Spy is used because i want to mock some methods inside the item I'm testing
-	 * InjectMocks uses dependency injection to insert the mock into the customer controller
-	 */
+	
 	@Spy
 	@InjectMocks
 	private CustomerController customerController;
 
-//	@Test
-//	public void readAllTest() {
-//		CustomerController customerController = new CustomerController(customerServices);
-//		List<Customer> customers = new ArrayList<>();
-//		customers.add(new Customer("Chris", "P"));
-//		customers.add(new Customer("Rhys", "T"));
-//		customers.add(new Customer("Nic", "J"));
-//		Mockito.when(customerServices.readAll()).thenReturn(customers);
-//		assertEquals(customers, customerController.readAll());
-//	}
+	@Test
+	public void readAllTest() {
+		CustomerController customerController = new CustomerController(customerServices);
+		List<Customer> customers = new ArrayList<>();
+		customers.add(new Customer("Chris", "P"));
+		customers.add(new Customer("Rhys", "T"));
+		customers.add(new Customer("Nic", "J"));
+		Mockito.when(customerServices.readAll()).thenReturn(customers);
+		assertEquals(customers, customerController.readAll());
+	}
 
-//	@Test
-//	public void createTest() {
-//		String firstName = "Chris";
-//		String surname = "Perrins";
-//		Mockito.doReturn(firstName, surname).when(customerController).getInput();
-//		Customer customer = new Customer(firstName, surname);
-//		Customer savedCustomer = new Customer(1L, "Chris", "Perrins");
-//		Mockito.when(customerServices.create(customer)).thenReturn(savedCustomer);
-//		assertEquals(savedCustomer, customerController.create());
-//	}
+	@Test
+	public void createTest() {
+		String firstName = "Chris";
+		String surname = "Perrins";
+		Mockito.doReturn(firstName, surname).when(customerController).getInput();
+		Customer customer = new Customer(firstName, surname);
+		Customer savedCustomer = new Customer(1L, "Chris", "Perrins");
+		Mockito.when(customerServices.create(customer)).thenReturn(savedCustomer);
+		assertEquals(savedCustomer, customerController.create());
+	}
 
-	/**
-	 * 
-	 */
+	
 	@Test
 	public void updateTest() {
 		String id = "1";
@@ -70,9 +63,7 @@ public class CustomerControllerTest {
 	}
 	
 
-	/**
-	 * Delete doesn't return anything, so we can just verify that it calls the delete method
-	 */
+	
 	@Test
 	public void deleteTest() {
 		String id = "1";
